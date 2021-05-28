8125ea8a4a8b132e4f01e0ac4660815d64d01b55
yetifunk <charlesdemar87@gmail.com>
2021-05-28 07:34:35 -0600

    second fizzbuzz solution

--- fizzbuzz/fizz.js
+++ fizzbuzz/fizz.js
@@ -1,11 +1,27 @@
+////First Solution
+
 for (let i = 1; i <= 100; i++) {
     if ((i % 5 === 0) && (i % 3 === 0)) {
-        console.log("FizzBuzz")
+        console.log("FizzBuzz");
     } else if (i % 5 === 0) {
-        console.log("Buzz")
+        console.log("Buzz");
     } else if (i % 3 === 0) {
-        console.log("Fizz") 
+        console.log("Fizz") ;
     } else {
-        console.log(i)
+        console.log(i);
+    };
+};
+
+////Second Solution
+
+for (let i = 1; i < 101; i++) {
+    if (i % 15 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz"); 
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
     }
-}
+}   
