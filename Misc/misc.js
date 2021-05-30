fed1f9fff95b7e593f6c57cd6525f4556417bee6
yetifunk <charlesdemar87@gmail.com>
2021-05-30 09:08:55 -0600

    misc problems

---
+++ Misc/misc.js
@@ -0,0 +1,15 @@
+// Reverse a string
+
+// Reverse a sentence ("bob likes dogs" -> "dogs likes bob")
+
+// Find the minimum value in a list
+
+// Find the maximum value in a list
+
+// Calculate a remainder (given a numerator and denominator)
+
+// Return distinct values from a list including duplicates (i.e. "1 3 5 3 7 3 1 1 5" -> "1 3 5 7")
+
+// Return distinct values and their counts (i.e. the list above becomes "1(3) 3(3) 5(2) 7(1)")
+
+// Given a string of expressions (only variables, +, and -) and a set of variable/value pairs (i.e. a=1, b=7, c=3, d=14) return the result of the expression ("a + b+c -d" would be -3).
