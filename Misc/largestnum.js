b2d36ee910365583f595373e89deedc813a9e256
yetifunk <charlesdemar87@gmail.com>
2021-06-03 08:26:09 -0600

    code golf

--- Misc/largestnum.js
+++ Misc/largestnum.js
@@ -12,20 +12,22 @@
 // Modify your function, so magnitude can be negative. If magnitude is negative, 
 // return the largest possible number with that many digits after a decimal point.
 
-// function largestNumber(num) {
-//     const nine = "9";
-//     if (num === 0) {
-//         console.log(parseInt("0"))
-//     } else if (num < 0) {
-//         console.log("." + nine.repeat(Math.abs(num)));
-//     } else {
-//         console.log(nine.repeat(num));
-//     };
-// };
-// largestNumber(5);
-// largestNumber(3);
-// largestNumber(0);
-// largestNumber(-5);
+function largestNumber(num) {
+    const nine = "9";
+    if (num === 0) {
+        console.log(parseInt("0"))
+    } else if (num < 0) {
+        console.log(parseFloat("." + nine.repeat(Math.abs(num))));
+    } else {
+        console.log(parseInt(nine.repeat(num)));
+    };
+};
+largestNumber(5);
+largestNumber(3);
+largestNumber(0);
+largestNumber(-5);
+
+//// Lets Play Code Golf
 
 function large(num) {
     return num > 0 ? parseInt("9".repeat(num)) : num === 0 ? 0 : parseFloat("." + "9".repeat(Math.abs(num)));
