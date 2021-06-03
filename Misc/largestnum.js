22cc4745e40575edf4fc6e2b0dc65212c164c948
yetifunk <charlesdemar87@gmail.com>
2021-06-03 07:58:55 -0600

    one liner for largest number

--- Misc/largestnum.js
+++ Misc/largestnum.js
@@ -12,18 +12,28 @@
 // Modify your function, so magnitude can be negative. If magnitude is negative, 
 // return the largest possible number with that many digits after a decimal point.
 
-function largestNumber(num) {
-    const nine = "9";
-    if (num === 0) {
-        console.log("0")
-    } else if (num < 0) {
-        console.log("." + nine.repeat(Math.abs(num)));
-    } else {
-        console.log(nine.repeat(num));
-    };
+// function largestNumber(num) {
+//     const nine = "9";
+//     if (num === 0) {
+//         console.log(parseInt("0"))
+//     } else if (num < 0) {
+//         console.log("." + nine.repeat(Math.abs(num)));
+//     } else {
+//         console.log(nine.repeat(num));
+//     };
+// };
+// largestNumber(5);
+// largestNumber(3);
+// largestNumber(0);
+// largestNumber(-5);
+
+function large(num) {
+    return num > 0 ? parseInt("9".repeat(num)) : num === 0 ? 0 : parseFloat("." + "9".repeat(Math.abs(num)));
 };
 
-largestNumber(5);
-largestNumber(3);
-largestNumber(0);
-largestNumber(-5);
+console.log(large(2))
+console.log(large(-2))
+console.log(large(0))
+
+
+
