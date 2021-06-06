8f823d75ff1c2bf1c4c76f32b4c9ae3499ff89cb
yetifunk <charlesdemar87@gmail.com>
2021-06-05 21:58:14 -0600

    unique values algo

--- Misc/misc.js
+++ Misc/misc.js
@@ -24,6 +24,16 @@ remainder(12,5)
 
 // Return distinct values from a list including duplicates (i.e. "1 3 5 3 7 3 1 1 5" -> "1 3 5 7")
 
+function onlyUnique(value, index, self) {
+    return self.indexOf(value) === index;
+  }
+  
+  // usage example:
+  var a = ['a', 1, 'a', 2, '1'];
+  var unique = a.filter(onlyUnique);
+  
+  console.log(unique); // ['a', 1, 2, '1']
+
 // Return distinct values and their counts (i.e. the list above becomes "1(3) 3(3) 5(2) 7(1)")
 
 // Given a string of expressions (only variables, +, and -) and a set of variable/value pairs (i.e. a=1, b=7, c=3, d=14) return the result of the expression ("a + b+c -d" would be -3).
