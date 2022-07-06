1a5198a2ac9df5ddf4170cf541c11c0b87fa8a19
yetifunk <charlesdemar87@gmail.com>
2022-07-06 09:52:54 -0600

    straynum and maxmin - codewars

---
+++ Misc/straynum.js
@@ -0,0 +1,16 @@
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+
+// Examples
+// [1, 1, 2] ==> 2
+// [17, 17, 3, 17, 17, 17, 17] ==> 3
+
+function stray(numbers) {
+    let max = Math.max(...numbers);
+    let min = Math.min(...numbers);
+
+    return numbers.filter(i => i === max).length === 1 ? max : min;
+}
